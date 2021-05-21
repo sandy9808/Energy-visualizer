599b503b92584702753db74d3ece40307b36044a
Sandeep kumar <sandeepkumar0702000@gmail.com>
2021-05-22 00:39:56 +0530

    Changes made

--- script.js
+++ script.js
@@ -5,23 +5,62 @@ const mong = require("mongoose");
 const app = express();
 
 var path = require('path');
+var sessiion = require('express-session');
+var flush = require('connect-flash');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(sessiion({
+  secret: 'secret',
+  cookie: {maxAge : 60000},
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(flush());
+
 //
 // mongoose.connect("mongodb://localhost:27017/login",{ useNewUrlParser: true });
 //
+
+
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
   first:String,
   last:String,
   email:String,
+  contact:String,
+  DOB:String,
+  age:String,
+  address:String,
+  admin:Number,
 });
 
 const User = mongoose.model("User",userSchema);
+
+
+
+
+
+// const user = new User({
+    // username: "iib2019016",
+    // password: "iiita123",
+    // first: "Ashish",
+    // last: "Tyagi",
+    // email: "iib2019016@iiita.ac.in",
+    // contact:"781xxxx50",
+    // DOB:"04/16/2000",
+    // age :"20",
+    // address : "B-93, XYZ colong, ABC city",
+    // admin:1,
+// });
+//
+// user.save();
+
+
 
 const userSchema2 = new mong.Schema({
   Datetime : String,
@@ -30,23 +69,11 @@ const userSchema2 = new mong.Schema({
 
 const User2 = mong.model("consumption",userSchema2);
 
-
-
-
-
-
-// const user = new User({
-//     username: "iib2019005",
-//     password: "iiita123",
-//     first: "Sandeep",
-//     last: "Kumar",
-//     email: "iib2019005@iiita.ac.in"
-// });
-//
-// user.save();
+// -------------------------------------------------------------------------------------------------------------
+// login page
 
 app.get("/",function(req,res){
-  res.sendFile(__dirname + "/login.html");
+  res.render("login",{message : req.flash('message')});
 })
 
 app.post("/",function(req,res){
@@ -56,7 +83,7 @@ app.post("/",function(req,res){
  console.log(passWord);
 
 
- mongoose.connect("mongodb://localhost:27017/login",{ useNewUrlParser: true });
+ mongoose.connect("mongodb://localhost:27017/login",{poolSize: 10, bufferMaxEntries: 0, useNewUrlParser: true,useUnifiedTopology: true});
 
   User.exists({username:userName,password:passWord},function (err, docs) {
     mongoose.connection.close();
@@ -66,28 +93,124 @@ app.post("/",function(req,res){
     }
     else{
       if(docs==true){
-        res.redirect("/home/"+userName);
-        console.log("First function call : ", docs);
+        req.flash('message','Welcome '+ userName);
+        // console.log("/home/" + userName + "." + 0);
+        res.redirect("/home/" + userName);
+        console.log("Accepted");
       }
       else{
-      res.status(400);
-       res.send('Current password does not match');
+        console.log("Rejected");
+        req.flash('message','Wrong Username/Password');
+
+        // alert('Wrong Username/Password');
+        res.redirect("/");
       }
     }
 });
 
 })
 
+//-------------------------------------------------------------------------------------------------------------
+// signup page
+
+app.get("/signup",function(req,res){
+  res.render("signup",{message : req.flash('message')});
+
+})
+
+app.post("/signup",function(req,res){
+  // console.log(req);
+  var username = req.body.username;
+  var first = req.body.first;
+  var last = req.body.last;
+  var email = req.body.email;
+  var DOB = req.body.DOB;
+  var contact = req.body.contact;
+  var address = req.body.address;
+  var password = req.body.password;
+
+    mongoose.connect("mongodb://localhost:27017/login",{poolSize: 10, bufferMaxEntries: 0, useNewUrlParser: true,useUnifiedTopology: true});
+    var user = new User({username:username,first:first,last:last,email:email,contact:contact,address:address,password:password,DOB:DOB,admin:0});
+    user.save();
+    res.redirect("/home/"+username);
+
+  });
+
+
+
+    // user.save(function(err,result) {
+    //   mongoose.connection.close();
+    //
+    //     if (err) {
+    //       res.send(err);
+    //     } else {
+    //       // console.log(result);
+    //       res.redirect("/profile/"+username);
+    //     }
+    //   });
+
+
+
+
+//-------------------------------------------------------------------------------------------------
+// home page
+
 app.get('/home/:username',function(req,res){
   const {username} = req.params;
-  res.render("home",{username:username});
+  // var val = req.params.value;
+
+  // console.log(username);
+
+
+  mongoose.connect("mongodb://localhost:27017/login",{poolSize: 10, bufferMaxEntries: 0,useNewUrlParser: true,useUnifiedTopology: true});
+
+   User.exists({username:username,admin:0},function (err, docs) {
+     mongoose.connection.close();
+
+     if (err){
+         console.log(err);
+     }
+     else{
+       if(docs==true){
+         res.render("home",{username:username,message : req.flash('message')});
+         // console.log("First function call : ", docs);
+       }
+       else{
+         res.render("homeadmin",{username:username,message : req.flash('message')});
+       }
+     }
+ });
+
 })
+
+
+//----------------------------------------------------------------------------------------------
+// profile page
 
 app.get('/profile/:username',function(req,res){
   const {username} = req.params;
-  console.log(username);
+  var admin;
 
-  mongoose.connect("mongodb://localhost:27017/login",{ useNewUrlParser: true });
+  // console.log(username);
+
+  mongoose.connect("mongodb://localhost:27017/login",{poolSize: 10, bufferMaxEntries: 0, useNewUrlParser: true,useUnifiedTopology: true});
+
+  User.exists({username:username,admin:0},function (err2, docs2) {
+    mongoose.connection.close();
+
+    if (err2){
+        console.log(err2);
+    }
+    else{
+      if(docs2==true){
+          admin=0;
+      }
+      else{
+         admin=1;
+      }
+    }
+ });
+
 
 
   User.find({username:username},function(err,docs){
@@ -97,33 +220,85 @@ app.get('/profile/:username',function(req,res){
         console.log(err);
     }
     else {
-      res.render("profile",{first:docs[0].first,username:docs[0].username,email:docs[0].email,last:docs[0].last});
+      if(admin==0)
+      res.render("profile",{message : req.flash('message'),first:docs[0].first,username:docs[0].username,email:docs[0].email,last:docs[0].last,address:docs[0].address,DOB:docs[0].DOB,contact:docs[0].contact});
+      else
+      res.render("profileadmin",{message : req.flash('message'),first:docs[0].first,username:docs[0].username,email:docs[0].email,last:docs[0].last,address:docs[0].address,DOB:docs[0].DOB,contact:docs[0].contact});
     }
   })
-
-
-
 })
+
+
+//-----------------------------------------------------------------------------------------------
+// view graphs portal
 
 app.get("/view/:username",function(req,res){
   const {username} = req.params;
-  res.render("graph",{username:username});
+
+  var admin;
+
+  // console.log(username);
+
+  mongoose.connect("mongodb://localhost:27017/login",{poolSize: 10, bufferMaxEntries: 0, useNewUrlParser: true,useUnifiedTopology: true});
+
+  User.exists({username:username,admin:0},function (err2, docs2) {
+    mongoose.connection.close();
+
+    if (err2){
+        console.log(err2);
+    }
+    else{
+      if(docs2==true){
+        res.render("graph",{username:username,message : req.flash('message')});
+      }
+      else{
+           res.render("graphadmin",{username:username,message : req.flash('message')});
+      }
+    }
+  });
 })
 
 app.post("/view/:username",function(req,res){
-  var username = req.params.username;
-  var startDate = req.body.startDate,startTime = req.body.startTime,endDate = req.body.endDate,endTime = req.body.endTime;
-   res.redirect("/barchart/"+startDate+"."+startTime+"."+endDate+"."+endTime+"."+username);
+  // console.log(req);
+  const {username} = req.params;
+  var startDate = req.body.startDate,startTime = req.body.startTime,endDate = req.body.endDate,endTime = req.body.endTime,cost=req.body.cost;
+   res.redirect("/barchart/"+startDate+"."+startTime+"."+endDate+"."+endTime+"."+username+"."+cost);
 })
 
 
-app.get("/barchart/:startDate.:startTime.:endDate.:endTime.:username",function(req,res){
+//------------------------------------------------------------------------------------------------------------
+// graph show
+app.get("/barchart/:startDate.:startTime.:endDate.:endTime.:username.:cost",function(req,res){
   var label=[],data=[];
    var username = req.params.username;
    var starttime = req.params.startTime;
    var endtime = req.params.endTime;
    var startdate = req.params.startDate;
    var enddate = req.params.endDate;
+   var cost = req.params.cost;
+
+  //  var admin;
+  //
+  //  mongoose.connect("mongodb://localhost:27017/login",{ useNewUrlParser: true });
+  //
+  //  User.exists({username:username,admin:0},function (err2, docs2) {
+  //    mongoose.connection.close();
+  //
+  //    if (err2){
+  //        console.log(err2);
+  //    }
+  //    else{
+  //      if(docs2==true){
+  //          admin=0;
+  //      }
+  //      else{
+  //         admin=1;
+  //      }
+  //    }
+  // });
+
+
+
 
    var start = "2005-" + startdate.substr(5,5) + " " + starttime.substr(0,2) + ":00:00";
    var end = "2005-" + enddate.substr(5,5) + " " + endtime.substr(0,2) + ":00:00";
@@ -133,9 +308,7 @@ app.get("/barchart/:startDate.:startTime.:endDate.:endTime.:username",function(r
    console.log(end);
 
 
-   mong.connect("mongodb://localhost:27017/energy",{ useNewUrlParser: true });
-
-
+   mong.connect("mongodb://localhost:27017/energy",{poolSize: 10, bufferMaxEntries: 0, useNewUrlParser: true,useUnifiedTopology: true});
 
 
 
@@ -161,12 +334,186 @@ app.get("/barchart/:startDate.:startTime.:endDate.:endTime.:username",function(r
        data.push(item.AEP_MW);
       }
 
-      res.render("chart",{labels:label,data:data,sum:sum,maxa:maxa,mini:mini,username:username});
+      cost = cost * sum;
 
+      // if(admin==0)
+      res.render("chart",{labels:label,data:data,sum:sum,maxa:maxa,mini:mini,username:username,cost:cost,message : req.flash('message')});
+      // else
+      // res.render("chartadmin",{labels:label,data:data,sum:sum,maxa:maxa,mini:mini,username:username,cost:cost});
      }
    })
 
 })
+
+
+
+
+
+//-----------------------------------------------------------------------------------------------------
+// update raw data
+
+app.get("/Update/:username",function(req,res){
+  const {username} = req.params;
+
+  res.render("updateadmin",{username:username,message : req.flash('message')});
+
+})
+
+app.post("/Update/:username",function(req,res){
+  // console.log(req);
+  const {username} = req.params;
+  var startdate = req.body.Date;
+  var starttime = req.body.Time;
+  var value = req.body.Value;
+
+  // console.log(starttime);
+  // console.log(startdate);
+  // console.log(value);
+
+   var start = "2005-" + startdate.substr(5,5) + " " + starttime.substr(0,2) + ":00:00";
+
+   mong.connect("mongodb://localhost:27017/energy",{poolSize: 10, bufferMaxEntries: 0,  useNewUrlParser: true,useUnifiedTopology: true});
+
+User2.updateOne({Datetime:start}, {AEP_MW:value}, function(err,result) {
+  mong.connection.close();
+
+    if (err) {
+      res.send(err);
+    } else {
+      console.log("Value Updated");
+      req.flash('message','Value Updated');
+      res.redirect("/Update/"+username);
+    }
+  });
+})
+
+// -----------------------------------------------------------------------------------------------------------------------
+// edit profile
+
+app.get("/editprofile/:username",function(req,res){
+  const {username} = req.params;
+  var admin;
+
+  // console.log(username);
+
+  mongoose.connect("mongodb://localhost:27017/login",{poolSize: 10, bufferMaxEntries: 0, useNewUrlParser: true,useUnifiedTopology: true});
+
+  User.exists({username:username,admin:0},function (err2, docs2) {
+    mongoose.connection.close();
+
+    if (err2){
+        console.log(err2);
+    }
+    else{
+      if(docs2==true){
+          admin=0;
+      }
+      else{
+         admin=1;
+      }
+    }
+ });
+
+
+
+  User.find({username:username},function(err,docs){
+    mongoose.connection.close();
+
+    if (err){
+        console.log(err);
+    }
+    else {
+      if(admin==0)
+      res.render("edit",{message : req.flash('message'),first:docs[0].first,username:docs[0].username,email:docs[0].email,last:docs[0].last,address:docs[0].address,DOB:docs[0].DOB,contact:docs[0].contact});
+      else
+      res.render("editadmin",{message : req.flash('message'),first:docs[0].first,username:docs[0].username,email:docs[0].email,last:docs[0].last,address:docs[0].address,DOB:docs[0].DOB,contact:docs[0].contact});
+    }
+  })
+})
+
+app.post("/editprofile/:username",function(req,res){
+  // console.log(req);
+  const {username} = req.params;
+  var first = req.body.first;
+  var last = req.body.last;
+  var email = req.body.email;
+  var DOB = req.body.DOB;
+  var contact = req.body.contact;
+  var address = req.body.address;
+  var password = req.body.password;
+
+    mongoose.connect("mongodb://localhost:27017/login",{poolSize: 10, bufferMaxEntries: 0,  useNewUrlParser: true,useUnifiedTopology: true});
+
+    User.updateOne({username:username},{first:first,last:last,email:email,contact:contact,address:address,password:password,DOB:DOB}, function(err,result) {
+      mongoose.connection.close();
+
+        if (err) {
+          res.send(err);
+        } else {
+          console.log("Profile Updated");
+          req.flash('message','Profile Updated');
+
+          res.redirect("/profile/"+username);
+        }
+      });
+})
+
+
+//--------------------------------------------------------------------------------------
+app.get("/RawView/:username",function(req,res){
+  const {username} = req.params;
+
+  mongoose.connect("mongodb://localhost:27017/login",{poolSize: 10, bufferMaxEntries: 0,  useNewUrlParser: true,useUnifiedTopology: true});
+
+  User.exists({username:username,admin:0},function (err2, docs2) {
+    mongoose.connection.close();
+
+    if (err2){
+        console.log(err2);
+    }
+    else{
+      if(docs2==true){
+        res.render("view_data",{username:username,message : req.flash('message')});
+      }
+      else{
+           res.render("view_dataadmin",{username:username,message : req.flash('message')});
+      }
+    }
+  });
+})
+
+app.post("/RawView/:username",function(req,res){
+  // console.log(req);
+  const {username} = req.params;
+  var startdate = req.body.Date;
+  var starttime = req.body.Time;
+  console.log(username);
+  console.log(startdate);
+  console.log(starttime);
+
+   var start = "2005-" + startdate.substr(5,5) + " " + starttime.substr(0,2) + ":00:00";
+
+   mong.connect("mongodb://localhost:27017/energy",{poolSize: 10, bufferMaxEntries: 0, useNewUrlParser: true,useUnifiedTopology: true});
+
+User2.find({Datetime:start}, function(err,result) {
+  mong.connection.close();
+
+    if (err) {
+      res.send(err);
+    } else {
+      console.log(result[0].AEP_MW);
+      req.flash('message',"Value is " + result[0].AEP_MW);
+
+      res.redirect("/RawView/"+username);
+    }
+  });
+})
+
+
+
+
+
+
 
 
 
